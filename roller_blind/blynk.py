d853505659d7939541011210ee89eeaabd7dad6f
Robin Wijnant <robin.wijnant@inthepocket.mobi>
2019-11-03 22:47:27 +0100

    implemented logger

--- roller_blind/blynk.py
+++ roller_blind/blynk.py
@@ -1,11 +1,13 @@
 import os
+import logging
 import blynklib
 
 blynk = blynklib.Blynk(os.getenv('BLYNK_TOKEN'))
+logger = logging.getLogger('blynk')
 
 @blynk.handle_event('write V10')
 def write_virtual_pin_handler(pin, value):
-    print(f'Slider value: {value}')
+    logger.INFO(f'New slider value: {value}')
     
 while True:
     blynk.run()
