da5ccd69b7a194e947d147e213d9e751d544c725
Robin Wijnant <robin.wijnant@inthepocket.mobi>
2019-12-01 12:08:10 +0100

    add a concept test for the hall sensor

---
+++ roller_blind/tests/hall-sensor-test.py
@@ -0,0 +1,20 @@
+import time
+import board
+import busio
+import adafruit_ads1x15.ads1015 as ADS
+from adafruit_ads1x15.analog_in import AnalogIn
+
+i2c = busio.I2C(board.SCL, board.SDA)
+ads = ADS.ADS1115(i2c)
+
+# Create single-ended input on channel 0
+channel = AnalogIn(ads, ADS.P0)
+
+# Create differential input between channel 0 and 1
+#chan = AnalogIn(ads, ADS.P0, ADS.P1)
+
+print("{:>5}\t{:>5}".format('raw', 'v'))
+
+while True:
+    print("{:>5}\t{:>5.3f}".format(chan.value, chan.voltage))
+    time.sleep(0.5)
