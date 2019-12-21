bf46e3885085de10e87b1eb718cfea34437575e8
Robin Wijnant <robin.wijnant@inthepocket.mobi>
2019-12-21 14:34:57 +0100

    blynk array to value

--- roller_blind/blynk.py
+++ roller_blind/blynk.py
@@ -9,11 +9,12 @@ roller_blind = RollerBlind()
 
 @blynk.handle_event('write V10')
 def update_position(pin, value):
-    logger.info(f'Set new position: {value}%')
-    roller_blind.roll(value)
+    logger.info(f'Set new position: {value[0]}%')
+    roller_blind.roll(value[0])
 
 @blynk.handle_event('write V11')
 def calibrate(pin, value):
+    if (value[0] == 0): return
     logger.info(f'Calibrate: {value}%')
 
 while True:
