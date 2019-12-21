197f80377645456f2675f021198c9faf3f6208a9
Robin Wijnant <robin.wijnant@inthepocket.mobi>
2019-12-21 14:23:25 +0100

    renamed files to underscrore

--- roller_blind/blynk.py
+++ roller_blind/blynk.py
@@ -1,13 +1,16 @@
 import os
 import logging
 import blynklib
+from roller_blind import RollerBlind
 
 blynk = blynklib.Blynk(os.getenv('BLYNK_TOKEN'))
 logger = logging.getLogger('blynk')
+roller_blind = RollerBlind()
 
 @blynk.handle_event('write V10')
 def write_virtual_pin_handler(pin, value):
     logger.info(f'Set new position: {value}%')
-    
+    roller_blind.roll(value)
+
 while True:
     blynk.run()
