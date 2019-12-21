493f94e8c88779c37ef3723343e86c94551d5e3a
Robin Wijnant <robin.wijnant@inthepocket.mobi>
2019-12-21 23:28:03 +0100

    update blynk event handlers

--- roller_blind/blynk.py
+++ roller_blind/blynk.py
@@ -8,14 +8,23 @@ logger = logging.getLogger('blynk')
 roller_blind = RollerBlind()
 
 @blynk.handle_event('write V10')
-def update_position(pin, value):
-    logger.info(f'Set new position: {value[0]}%')
-    roller_blind.roll(value[0])
+def handle_update_position(pin, value):
+    logger.info(f'Setting new position ({value[0]}%)...')
+    roller_blind.roll(int(value[0]))
+    logger.info('New position reached')
+
+def calibrate():
+    logger.info('Calibrating...')
+    roller_blind.calibrate()
+    logger.info('Calibration completed')
 
 @blynk.handle_event('write V11')
-def calibrate(pin, value):
-    if (value[0] == 0): return
-    logger.info(f'Calibrate: {value}%')
+def handle_calibrate(pin, value):
+    if (not int(value[0])): return
+    calibrate()
+
+calibrate()
+blynk.virtual_write('V10', 0)
 
 while True:
     blynk.run()
