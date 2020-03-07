d76dfcef675fc10f3ed9a979a066b5b4f03c8b5b
Robin Wijnant <robin.wijnant@inthepocket.mobi>
2020-03-07 11:23:44 +0100

    fix import in main

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from dotenv import load_dotenv
-load_dotenv()
 
 import logger
 
+load_dotenv()
+
+
 try:
-    import roller_blind.blynk
+    import src.blynk
 except KeyboardInterrupt:
     print()
