de0b3cdb276ce12023943aa71e1675198c271e2a
Robin Wijnant <robin.wijnant@inthepocket.mobi>
2020-03-15 22:38:18 +0100

    update hall sensor test for detect function

--- test/test_hall_sensor.py
+++ test/test_hall_sensor.py
@@ -11,10 +11,10 @@ class HallSensorTest(unittest.TestCase):
     def setUp(self):
         self.sensor = HallSensor(board.SCL, board.SDA)
 
-    def test_read_strength(self):
+    def test_detect(self):
         try:
             while True:
-                print(self.sensor.read_strength())
+                print(self.sensor.detect())
                 time.sleep(0.5)
         except KeyboardInterrupt:
             pass
