b63c42b8cc30195a4fc1329424aaf22426ea0872
Robin Wijnant <robin.wijnant@inthepocket.mobi>
2020-03-16 21:16:36 +0100

    log position value from blynk servers

--- main.py
+++ main.py
@@ -37,6 +37,7 @@ def handle_update_position(pin, value):
     if (has_synced[VirtualPin.POSITION.value] == False):
         roller_blind.position = value
         has_synced[VirtualPin.POSITION.value] = True
+        logger.info(value)
         logger.info(f'New position set ({value[0]}%)')
         return
 
