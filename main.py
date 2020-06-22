39bab7a9d6eae46d1d929ae37456e918ff4916c6
Robin Wijnant <robin.wijnant@inthepocket.mobi>
2020-06-22 08:21:53 +0200

    print coroutine

--- main.py
+++ main.py
@@ -36,6 +36,7 @@ def run(coroutine, success_message):
         current_task.cancel()
     except UnboundLocalError:
         pass
+    print(coroutine)
     loop = asyncio.new_event_loop()
     current_task = asyncio.run_coroutine_threadsafe(coroutine, loop)
     current_task.add_done_callback(lambda task: logger.info(success_message))
