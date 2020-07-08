5942c4e1fdbf0ddf080e6e19e140e39852ce3aa0
Robin Wijnant <robin.wijnant@inthepocket.mobi>
2020-07-08 19:28:41 +0200

    run task top level

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ def run(coroutine, success_message):
 
     current_task = asyncio.create_task(coroutine)
     current_task.add_done_callback(lambda task: logger.info(success_message))
-    await current_task
+    asyncio.run(current_task)
 
 
 def do_daily_roll(direction_up):
