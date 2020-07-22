58b676ebfb99246a6a84542c44c6ac7e0a625048
Robin Wijnant <robin.wijnant@inthepocket.mobi>
2020-07-22 20:44:27 +0200

    recalibrate hall sensor

--- src/components/hall_sensor.py
+++ src/components/hall_sensor.py
@@ -6,15 +6,14 @@ from adafruit_ads1x15.analog_in import AnalogIn
 
 
 class HallSensor:
-
     def __init__(self, scl_pin, sda_pin):
         i2c = busio.I2C(scl_pin, sda_pin)
         ads = ADS.ADS1015(i2c)
         self.channel = AnalogIn(ads, ADS.P0)
 
     def read_strength(self):
-        return self.channel.value / math.pow(2, 16) * 100 # [0,100]
+        return self.channel.value / math.pow(2, 16) * 100  # [0,100]
 
     def detect(self):
         value = self.read_strength()
-        return value < 21.5 or value > 23.5
+        return value < 20.4
