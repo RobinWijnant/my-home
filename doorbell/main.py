9eb890f8b74d6e7b797cea478802e0bd53a92379
Robin Wijnant <robin.wijnant@inthepocket.mobi>
2020-10-29 00:02:36 +0100

    init doorbell receiver

--- doorbell/main.py
+++ doorbell/main.py
@@ -9,19 +9,31 @@ from dotenv import load_dotenv
 sys.path.append("../packages")
 import ha_config
 import common.logger
+from common.thread_manager import ThreadManager
+from radio_switch import RadioSwitch
 
 load_dotenv()
 
 logger = logging.getLogger("home")
 client = mqtt.Client()
 topic = f"{os.getenv('MQTT_PREFIX')}/device_automation/{os.getenv('DEVICE_ID')}"
+r_switch = RadioSwitch()
+thread_manager = ThreadManager()
 
 
 def on_connect(client, userdata, flags, rc):
     logger.info(f"Connection established with MQTT broker")
     main_config = ha_config.get_main_button_config(topic)
     client.publish(f"{topic}/config", json.dumps(main_config))
-    client.publish(f"{topic}/press", "PRESS TEST")
+    thread_manager.execute(listen_click)
+
+
+def listen_click():
+    logger.info(f"Listening to doorbell button...")
+    while True:
+        if r_switch.is_available():
+            message = r_switch.read()
+            logger.info(f"New button press {json.dumps(message)}")
 
 
 try:
@@ -34,5 +46,6 @@ try:
 
 except KeyboardInterrupt:
     print()
+    thread_manager.stop()
     client.disconnect()
     logger.warning("Script interrupted by user")
---
+++ doorbell/radio_switch.py
@@ -0,0 +1,28 @@
+import board
+from rpi_rf import RFDevice
+
+
+class RadioSwitch:
+    DATA_PIN = 17
+
+    def __init__(self):
+        self.rf_device = RFDevice(RadioSwitch.DATA_PIN)
+        self.rf_device.enable_rx()
+        self.last_read_timestamp = None
+
+    def __del__(self):
+        self.rf_device.cleanup()
+
+    def is_available(self):
+        if self.rf_device.rx_code_timestamp != self.last_read_timestamp:
+            self.last_read_timestamp = self.rf_device.rx_code_timestamp
+            return True
+        return False
+
+    def read(self):
+        return {
+            "code": self.rf_device.rx_code,
+            "pulse_length": self.rf_device.rx_pulselength,
+            "protocol": self.rf_device.rx_protocol,
+        }
+
