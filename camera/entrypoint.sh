3a29ec94bf29eb01986efc695d72f636eaf37856
Robin Wijnant <robin.wijnant@inthepocket.mobi>
2020-11-11 17:23:23 +0100

    dont detect motion

--- camera/entrypoint.sh
+++ camera/entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 git pull origin $(git rev-parse --abbrev-ref HEAD)
 cp -af ./motion/. /etc/motion/
-motion -n
+
+# -n: don't start deamon in background
+# -m: don't detect motion
+motion -n -m
