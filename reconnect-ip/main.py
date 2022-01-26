6c6668b7675adf8993cda3ea6773eadeb77baff2
Robin Wijnant <robin.wijnant@inthepocket.mobi>
2022-01-26 23:29:15 +0100

    remove redirect

--- reconnect-ip/main.py
+++ reconnect-ip/main.py
@@ -7,10 +7,9 @@ load_dotenv()
 
 
 async def main():
-
-    browser = await launch(ignoreHTTPSErrors=True, headless=False)
+    browser = await launch(ignoreHTTPSErrors=True, headless=True)
     page = await browser.newPage()
-    await page.goto("https://192.168.1.10:8443")
+    await page.goto(os.getenv("UNIFI_CONTROLLER_URL"))
 
     # Login
     await page.waitForSelector("input[name=username]")
@@ -18,16 +17,17 @@ async def main():
     await page.type("input[name=username]", os.getenv("USERNAME"))
     await page.type("input[name=password]", os.getenv("PASSWORD"))
     await page.click("#loginButton")
-    await page.waitForNavigation()
 
     # Reconnect client with given ip
-    await page.goto("https://192.168.1.10:8443/manage/default/clients")
+    await page.waitForSelector("a[href='/manage/default/clients']")
+    await page.click("a[href='/manage/default/clients']")
     await page.waitForSelector(f"span[data-label='{os.getenv('IP_ADDRESS')}']")
     await page.click(f"span[data-label='{os.getenv('IP_ADDRESS')}']")
     await page.waitForSelector("button[name=reconnect-client]")
     await page.waitFor(1000)
     await page.click("button[name=reconnect-client]")
 
+    # Wait for the client to disappear
     await page.waitForFunction(
         f"!document.querySelector(`span[data-label='{os.getenv('IP_ADDRESS')}']`)"
     )
