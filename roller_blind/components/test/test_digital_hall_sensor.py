433b4bfcfa7ba7f68f92b3e861292a08dc72389b
Robin Wijnant <robin.wijnant@inthepocket.com>
2022-08-08 22:56:25 +0200

    update import

--- roller_blind/components/test/test_digital_hall_sensor.py
+++ roller_blind/components/test/test_digital_hall_sensor.py
@@ -1,7 +1,7 @@
 import time
 import unittest
 
-from components.digital_hall_sensor import DigitalHallSensor
+from roller_blind.components.digital_hall_sensor import DigitalHallSensor
 
 SENSOR_PIN = 25
 
